dc6769594f76c34b0eb4b565c41ba997eb9cd597
Frangia22 <giacherofranc22@gmail.com>
2025-06-02 11:14:21 -0300

    feat: :art: initialize Vite project with React and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+
+
+export default function App() {
+  return (
+    <main className="bg-[#F8FAFC] min-h-screen">
+      <h1 className="text-5xl font-bold text-red-500">Bienvenido a mi aplicación</h1>
+      <p>Esta es una aplicación de ejemplo utilizando React.</p>
+    </main>
+  );
+}
