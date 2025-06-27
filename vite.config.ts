46155687ff71cbbbacbcd1f9083bb10d94724180
Frangia22 <giacherofranc22@gmail.com>
2025-06-27 16:12:02 -0300

    feat: :sparkles: Configurar alias de rutas en tsconfig.json y vite.config.ts para mejorar la gestión de importaciones

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({  
     plugins: [
         react(),    
-        tailwindcss(),  
+        tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
 })
